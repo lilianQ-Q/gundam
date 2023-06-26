40043e4dcfb2c07681a661775f3c4ba3a716db1d
lilianQ_Q <lilianpierredamiens@gmail.com>
2023-06-26 15:50:36 +0200

    [IMP] gundam: try to activate cors

--- src/back/src/main.ts
+++ src/back/src/main.ts
@@ -4,12 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {rawBody: true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {rawBody: true, cors: true});
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.useBodyParser('json', { limit: '50mb' });
   app.useBodyParser('urlencoded', { extended: true, limit: '50mb' });
-
+  
   await app.listen(3000);
 }
 bootstrap();
